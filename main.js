222b446d2d296316c5c9c3c63c01c52961a9762d
coreyloftus <coreyloftus@gmail.com>
2022-11-04 11:28:13 -0400

    Add Variable Review Notes

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log (`js review loaded`)
+
+// Primatives
+const fullName = "Corey M Loftus"
+let age = 37
+let heightCM = 69.5
+// undefined
+// null / NaN
+
+// Objects
+    // Array
+    const faveColors = ['teal','red','navy','blue-green','gold']
+    console.log(faveColors)
+    faveColors.push('almond')
+    // ^^^ add an item to the end
+    faveColors.unshift('grass-green')
+    console.log(faveColors)
